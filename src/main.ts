6803b6dbf0560650717d69fecdcd267152545bfb
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-01-04 20:46:37 +0200

    add vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import App from '@/App.vue';
 import router from '@/router';
 import '@/assets/main.css';
 import BaseLoader from '@/components/ui/BaseLoader.vue';
+import vuetify from '@/vuetify';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(vuetify);
 app.component('BaseLoader', BaseLoader);
 
 app.mount('#app');
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
     <h1>This is an about page</h1>
+    <v-chip> Chip </v-chip>
+    <v-btn> Button </v-btn>
+    <v-tooltip location="top" text="Tooltip">
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props">Tooltip</v-btn>
+      </template>
+    </v-tooltip>
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" v-bind="props"> Activator slot </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="(item, index) in items" :key="index" :value="index">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const items = ref([
+  { title: 'Click Me' },
+  { title: 'Click Me' },
+  { title: 'Click Me' },
+  { title: 'Click Me 2' },
+]);
+</script>
