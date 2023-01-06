a805bb978e8b55a9db3ae2e395e7ef9e77ffeb33
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-01-06 23:03:53 +0200

    test vuetify

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>This is an about page</h1>
     <v-chip> Chip </v-chip>
-    <v-btn> Button </v-btn>
+    <v-btn class="pr-8 w-25" icon="mdi-star" color="secondary"></v-btn>
+    <v-btn prepend-icon="mdi-vuetify"> Button </v-btn>
     <v-tooltip location="top" text="Tooltip">
       <template v-slot:activator="{ props }">
         <v-btn v-bind="props">Tooltip</v-btn>
       </template>
     </v-tooltip>
+    <v-icon size="large" color="green-darken-2"> mdi-domain </v-icon>
     <v-menu>
       <template v-slot:activator="{ props }">
         <v-btn color="primary" v-bind="props"> Activator slot </v-btn>
--- src/vuetify/index.ts
+++ src/vuetify/index.ts
@@ -2,10 +2,19 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 });
 
 export default vuetify;
