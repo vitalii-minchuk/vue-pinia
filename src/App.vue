cd7d3560ecc7fb9fe8f0285b9855f30a5ac60a02
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-01-19 21:43:35 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,20 @@
 <template>
-  <RouterView />
+  <v-app>
+    <v-toolbar dark prominent>
+      <v-container>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Vuetify</v-toolbar-title>
+      </v-container>
+    </v-toolbar>
+    <v-main>
+      <v-container fluid>
+        <RouterView />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 </script>
-
-<style lang="css">
-.header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-}
-
-.header-logo {
-  max-width: 50px;
-  margin-right: 10px;
-}
-
-.btn {
-  border: none;
-  width: 100px;
-  height: 40px;
-  font-size: 14px;
-  margin: 0 10px;
-  border-radius: 10px;
-  cursor: pointer;
-  background: #efefef;
-}
-
-.btn:hover {
-  opacity: 0.7;
-}
-
-.btn_green {
-  background: #37df5c;
-}
-
-.tabs {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 30px;
-}
-</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,33 +1,4 @@
 <template>
-  <div>
-    <h1>This is an about page</h1>
-    <v-chip> Chip </v-chip>
-    <v-btn class="pr-8 w-25" icon="mdi-star" color="secondary"></v-btn>
-    <v-btn prepend-icon="mdi-vuetify"> Button </v-btn>
-    <v-tooltip location="top" text="Tooltip">
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props">Tooltip</v-btn>
-      </template>
-    </v-tooltip>
-    <v-menu>
-      <template v-slot:activator="{ props }">
-        <v-btn color="primary" v-bind="props"> Activator slot </v-btn>
-      </template>
-      <v-list>
-        <v-list-item v-for="(item, index) in items" :key="index" :value="index">
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-  </div>
+  <div></div>
 </template>
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const items = ref([
-  { title: 'Click Me' },
-  { title: 'Click Me' },
-  { title: 'Click Me' },
-  { title: 'Click Me 2' },
-]);
-</script>
+<script setup lang="ts"></script>
